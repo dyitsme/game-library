d9eb8ecf2e63f9c240737e313692f3bca5e5dae4
dyitsme <denzel_bryan_yongco@dlsu.edu.ph>
2022-04-29 21:21:54 +0800

    Added empty delete game pages

---
+++ client/src/views/DeleteGame1.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar></Navbar>
+  hello
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  name: 'DeleteGame1',
+  components: {
+    'Navbar': Navbar
+  }
+}
+</script>
+
+<style>
+</style>
