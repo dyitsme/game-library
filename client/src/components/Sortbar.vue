42917e6715a38b9d4a4a3da30421841828376c47
XSverdar <hythunderz@gmail.com>
2022-05-16 23:56:10 +0800

    Added images

---
+++ client/src/components/Sortbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="sort-btn">
+    <img class="icon" src="../assets/sort3_icon.svg">
+  </button>
+  <button class="sort-btn">
+    <img class="icon" src="../assets/sort2_icon.svg">
+  </button>
+  <button class="sort-btn">
+    <img class="icon" src="../assets/sort1_icon.svg">
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Sortbar'
+}
+</script>
+
+<style>
+  button {
+    background-color: Transparent;
+  }
+  .sort-btn {
+    float: right;
+  }
+
+  .icon {
+  height: 1.4em;
+  width: 1.4em;
+  }
+</style>
--- client/src/views/Store.vue
+++ client/src/views/Store.vue
@@ -3,19 +3,26 @@
   <div class="container">
     <h1>Store</h1>
     <Searchbar></Searchbar>
-    test
+    <Sortbar></Sortbar>
+    <div class="store-container">
+      <img src="../assets/games/csgo_icon.svg" height="100px" width="50px"><br>
+      CounterStrike: Global Offensive<br>
+      <img src="../assets/games/4_stars.svg" width="50px">
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
 import Searchbar from '../components/Searchbar.vue'
+import Sortbar from '../components/Sortbar.vue'
 
 export default {
   name: 'Store',
   components: {
-    'Navbar': Navbar,
-    'Searchbar': Searchbar
+    "Navbar": Navbar,
+    "Searchbar": Searchbar,
+    "Sortbar": Sortbar
   }
 }
 </script>
@@ -23,8 +30,21 @@ export default {
 <style>
   body {
     background-color: black;
+    color: white;
   }
+
+  h1 {
+    font-size: 36px;
+    color: white;
+  }
+
   .container {
-    margin: 50px 100px 50px 100px; /* top | right | bottom | left */
+    margin: 50px 100px 50px 100px;
+  }
+
+  .store-container {
+    margin-top: 25px;
+    font-size: 20px;
+    vertical-align: middle;
   }
 </style>
